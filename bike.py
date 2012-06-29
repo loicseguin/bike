5387acecd6de295ea35767da81b25e2283a111d2
Loïc Séguin-C <loicseguin@gmail.com>
2012-06-29 16:22:33 -0400

    Add French translation and all rides printing

--- bike.py
+++ bike.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 """
 bike
@@ -32,14 +33,36 @@ __version__ = '0.1'
 
 
 import argparse
+import locale
 import os
 import sys
 import time
 
 
 RIDEDB = os.path.expanduser('~/.bikerides')
-TIMESTR = "%d/%b/%Y-%H:%M:%S"
+TIMESTR = "%d/%m/%Y-%H:%M:%S"
 MINUTES_PER_HOUR = 60.
+
+
+FR_DICT = {
+    "Distance: %6.1f km": "Distance : %6.1f km",
+    "Duration: %6.1f h": "Durée : %6.1f h",
+    "Average speed: %6.2f km/h": "Vitesse moyenne : %6.2f km/h",
+    "Gather statistics about bike rides.":
+        "Collecte des données sur des randonnées à bicyclette.",
+    "add a new ride": "ajouter une nouvelle randonnée",
+    "print statistics about all rides": "imprimer les statistiques",
+    "Enter distance: ": "Entrer la distance : ",
+    "Enter duration: ": "Entrer la durée : ",
+    "Comment (optional): ": "Commentaire (optionnel) : ",
+    "stats": "stats",
+    "add": "add",
+    "Date": "Date",
+    "Distance": "Distance",
+    "Duration": "Durée",
+    "Comment": "Commentaire"
+    }
+TRANS_DICT = {}
 
 
 try:
@@ -47,6 +70,21 @@ try:
         input = raw_input
 except NameError:
     pass
+
+
+def init_locale():
+    """Set the locale to the user's preference."""
+    locale.setlocale(locale.LC_ALL, '')
+    lang = locale.getlocale()[0][:2]
+    global TRANS_DICT
+    if lang == 'fr':
+        TRANS_DICT = FR_DICT
+
+
+def _(astring):
+    """Translate ``astring`` to the current locale if translation is
+    available."""
+    return TRANS_DICT.get(astring, astring)
 
 
 def parse_duration(duration_str):
@@ -79,9 +117,9 @@ def add_ride(args):
 
     """
     timestamp = time.localtime()
-    distance = float(input("Enter distance: "))
-    duration = parse_duration(input("Enter duration: "))
-    comment = input("Comment (optional): ")
+    distance = float(input(_("Enter distance: ")))
+    duration = parse_duration(input(_("Enter duration: ")))
+    comment = input(_("Comment (optional): "))
 
     rides_file = open(RIDEDB, 'a')
     rides_file.write(' '.join([time.strftime(TIMESTR, timestamp),
@@ -106,40 +144,52 @@ def read_db_file():
 
 
 def print_stats(args):
-    """Print statistics about the rides.
-
-    """
+    """Print statistics about the rides."""
     rides = read_db_file()
     tot_distance = 0.0
     tot_duration = 0.0
     for ride in rides:
         tot_distance += ride['distance']
         tot_duration += ride['duration']
-    print("Distance: %6.1f km" % tot_distance)
-    print("Duration: %6.1f h" % tot_duration)
-    print("Average speed: %6.2f km/h" % (tot_distance / tot_duration))
+    print(_("Distance: %6.1f km") % tot_distance)
+    print(_("Duration: %6.1f h") % tot_duration)
+    print(_("Average speed: %6.2f km/h") % (tot_distance / tot_duration))
+
+
+def print_rides(args):
+    """Print all rides in database."""
+    rides = read_db_file()
+    print('{0:16s}  {1:13s}  {2:13s}  {3:30s}'.format(
+          _('Date'), _('Distance') + ' (km)', _('Duration') + ' (h)', _('Comment')))
+    print('{0:=<16s}  {1:=<13s}  {2:=<13s}  {3:=<30s}'.format('', '', '', ''))
+    for ride in rides:
+        print('{0:16s}  {1:13.1f}  {2:13.1f}  {3:30s}'.format(
+            time.strftime('%d/%m/%Y %H:%M', ride['timestamp']),
+            ride['distance'], ride['duration'], ride['comment']))
 
 
 def run(argv=sys.argv[1:]):
-    """Parse the command line arguments and run the appropriate command.
-
-    """
+    """Parse the command line arguments and run the appropriate command."""
     clparser = argparse.ArgumentParser(
-            description='Gather statistics about bike rides.')
+            description=_('Gather statistics about bike rides.'))
     clparser.add_argument('-v', '--version', action='version',
             version='%(prog)s ' + __version__)
 
     subparsers = clparser.add_subparsers()
-    statsparser = subparsers.add_parser('stats',
-            help='print statistics about all rides')
+    statsparser = subparsers.add_parser(_('stats'),
+            help=_('print statistics about all rides'))
     statsparser.set_defaults(func=print_stats)
 
-    addparser = subparsers.add_parser('add', help='add a new ride')
+    addparser = subparsers.add_parser(_('add'), help=_('add a new ride'))
     addparser.set_defaults(func=add_ride)
+
+    printparser = subparsers.add_parser(_('rides'), help=_('print all rides'))
+    printparser.set_defaults(func=print_rides)
 
     args = clparser.parse_args(argv)
     args.func(args)
 
 
 if __name__ == '__main__':
+    init_locale()
     run()
